566b9f6c2aa6e331a7d20bcfe898b7147f8e57bb
juangomez0120 <jmgomezortiz@gmail.com>
2022-04-20 10:20:18 -0500

    Cambios Nav-bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,17 +11,15 @@ const Navbar = ({selected}) => {
       <>
         <nav className="nav-horizontal">
             <img id='logo-nav' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Ternium_Logo.svg/1200px-Ternium_Logo.svg.png" alt="logo" />
-        </nav>
-        <nav className='nav-vertical'>
-          <Link to='/'>
+            <Link to='/'>
             <Button text='Subir Archivo' style={`btn ${'upload' === selected ? 'btn-selected' : ''}`} icon="cloud"/>
-          </Link>
-          <Link to='/Dashboard'>
-            <Button text='Dashboard' style={`btn ${'dashboard' === selected ? 'btn-selected' : ''}`} icon="chart"/>
-          </Link>
-          <Link to='/Historial'>
-            <Button text='Historial' style={`btn ${'history' === selected ? 'btn-selected' : ''}`} icon="history"/>
-          </Link>
+            </Link>
+            <Link to='/Dashboard'>
+              <Button text='Dashboard' style={`btn ${'dashboard' === selected ? 'btn-selected' : ''}`} icon="chart"/>
+            </Link>
+            <Link to='/Historial'>
+              <Button text='Historial' style={`btn ${'history' === selected ? 'btn-selected' : ''}`} icon="history"/>
+            </Link>
         </nav>
     </>
   )
