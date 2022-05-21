6092f70069069744687bfa45d88016be68da1e9d
facund015 <A01283666@itesm.mx>
2022-05-20 20:28:55 -0500

    File Upload Documentation

--- Django_API/API_PROJECT/api/tasks.py
+++ Django_API/API_PROJECT/api/tasks.py
@@ -1,5 +1,6 @@
 from API_PROJECT.celery import app
 from celery import shared_task
+from api.db import db 
 
 from django.core.files.storage import default_storage
 
@@ -10,154 +11,180 @@ from sklearn.preprocessing import LabelEncoder
 import time
 from os.path import exists
 
-# def get_columns(file):
-#     """Gets the columns of a file
-
-#     Args:
-#         file (file): File to get the columns from
-
-#     Returns:
-#         list: List with the columns
-#     """    ''''''
-#     for chunk in pd.read_csv(file, chunksize=1):
-#         return chunk.columns.to_list()
-
-# def filter_columns(df, columns):
-#     """Filters the columns of a dataframe
-
-#     Args:
-#         df (DataFrame): DataFrame to filter
-#         columns (List): List of columns to keep
-
-#     Returns:
-#         DataFrame: DataFrame with the columns filtered
-#     """
-#     return df[columns]
     
-def clean_df(df):
+def clean_df(df: pd.DataFrame):
     """Cleans a dataframe from null or NaN values
 
     Args:
         df (DataFrame): Dataframe to clean
-
-    Returns:
-        DataFrame: Cleaned dataframe
     """
+    # Iterates over the columns of the dataframe and cleans the data
     for column in df:
+        # Fils the null values with the a 'NA' string if at least one value is null
         if df[column].isna().sum() > 0:
             df[column] = df[column].fillna('NA')
 
-def encode_labels(df):
+def load_file_to_df(file_name: str, columns_IF: list) -> pd.DataFrame:
+    """loads a csv file to a dataframe
+
+    Args:
+        file_name (str): Name of the file to load
+        columns_IF (list): List of columns to keep in the dataframe
+
+    Returns:
+        DataFrame: Dataframe with the data
+    """    
+    # Creates a dataframe to store the data from the file
+    df_p = pd.DataFrame()
+    # Opens the file
+    file = default_storage.open(file_name)
+
+    # Reads the file in chunks of 10000 lines at a time and stores the data in the dataframe
+    for chunk in pd.read_csv(file, chunksize=10000, low_memory=False, usecols=columns_IF):
+        # Cleans the dataframe from null or NaN values
+        clean_df(chunk)
+        # Appends the cleaned chunk to the dataframe to store the data
+        df_p = pd.concat([df_p, chunk])
+    
+    # Closes the file
+    file.close()
+    # Deletes the file variable
+    del file
+    # Delete the file from the storage
+    default_storage.delete(file_name)
+    
+    # Returns the dataframe with the data
+    return df_p
+
+def encode_labels(df: pd.DataFrame) -> dict:
     """Encode labels of a dataframe
 
     Args:
         df (DataFrame): DataFrame with the data to categorize
         
-    Returns:
-        DataFrame: Dataframe with the encoded labels
+    returns:
+        dict: Dictionary with the label encoders
     """
+    # Creates a dictionary to store the label encoders
     les = dict()
+    # Iterates over the columns of the dataframe and encodes the labels
     for column in df:
+        # Creates a label encoder for the column and stores it in the dictionary
         les[column] = LabelEncoder()
+        # Encodes the labels and stores them in a new column in the dataframe
         df[str(column)+'_encoded'] = les[column].fit_transform(df[column].astype(str))
+        # Drops the original column
         df.drop(column, axis=1, inplace=True)
     return les
 
-def decode_labels(df, les):
+def decode_labels(df: pd.DataFrame, les: dict):
     """Decode labels of a dataframe
 
     Args:
         df (DataFrame): DataFrame with the data to categorize
-        
-    Returns:
-        DataFrame: Dataframe with the decoded labels
+        les (dict): Dictionary with the label encoders
     """
+    # Iterates over the columns of the dataframe and decodes the labels
     for column in df.iloc[:,:-1]:
+        # Gets name of the column without the encoded suffix
         name = column.split('_encoded')[0]
-        les[name].classes_
+        # Decodes the labels and stores them in a new column in the dataframe
         df[name] = les[name].inverse_transform(df[column])
+        # Drops the encoded column
         df.drop(column, axis=1, inplace=True)
 
-def train_model(df, model):
+def train_model(df: pd.DataFrame, model: IsolationForest):
     """Trains a model
 
     Args:
         df (DataFrame): Dataframe to train the model
-        model (Model): Model to train
+        model (IsolationForest): Model to train
     """
+    # Trains the model
     model.fit(df)
     
-def apply_model(df, model):
-    """Applies the Isolation Forest algorithm to the dataframe
+def apply_model(df: pd.DataFrame, model: IsolationForest) -> pd.DataFrame:
+    """Applies a trained Isolation Forest algorithm to the dataframe
 
     Args:
         df (DataFrame): DataFrame with the data to classify
-        model (Model): Model to apply
+        model (IsolationForest): Model to apply
         
     Returns:
         DataFrame: DataFrame with the data classified
     """
+    # Applies the model to the dataframe and adds the result to the dataframe
     df['anomaly_scores'] = model.decision_function(df)  
 
-def upload_to_db(df, collection):
+def upload_to_db(df: pd.DataFrame, file_name: str, base_file_name: str, date: str, internal_attributes: list, external_attributes: list):
     """Uploads the dataframe to a database
 
     Args:
         df (DataFrame): Dataframe to upload
-        collection (Collection): Collection to upload to
+        file_name (str): Name of the file to upload
+        base_file_name (str): Name of the file without extension and date
+        date (str): Date of the file
+        internal_attributes (list): List of internal attributes
+        external_attributes (list): List of external attributes
     """
-    for index, row in df.iterrows():
-        collection.insert_one(row.to_dict())
+    # Gets collections references from database
+    history_collection = db["RunHistory"]
+    file_data_collection = db["FileData"]
+    
+    # Creates a new document in the history collection with the data of the run
+    history_collection.insert_one({
+        "_id" : file_name,
+        "base_file_name" : base_file_name,
+        "date" : date,
+        "internal_attributes" : internal_attributes,
+        "external_attributes" : external_attributes
+    })
+    
+    # Creates a new document in the file data collection with the data of the processed file
+    file_data_collection.insert_one({
+        "_id" : file_name,
+        "data" : df.to_dict(orient='list')
+    })
 
 @shared_task
-def process_file(file_name, columns_IF):
-    """Applies the Isolation Forest algorithm to the file using certain columns
+def process_file(file_name: str, base_file_name: str, date: str, internal_attributes: list, external_attributes: list):
+    """Reads a csv file, cleans it applies an Isolation Forest algorithm and uploads the data to a database 
 
     Args:
-        file (file): File to be processed
-        columns (list): List of columns in the file
-        columns_IF (list): List of columns used by the Isolation Forest algorithm
-
-    Returns:
-        : None
+        file_name (str): Name of the file to process
+        base_file_name (str): Name of the file without extension and date
+        date (str): Date of the file
+        internal_attributes (list): List of internal attributes to keep
+        external_attributes (list): List of external attributes to keep
     """
-    #file = default_storage.open(file_name)
-    #columns = get_columns(file)
-    # print(columns)
-    model = IsolationForest(random_state=0, max_features=1, max_samples=91, n_estimators=400, contamination=0.48)
-    chunk_size = 10000
-    df_p = pd.DataFrame()
+    # Combines internal and external attributes to be used in the model
+    columns_IF = internal_attributes + external_attributes
+    # Creates model
+    model = IsolationForest()
+    
+    #loads the csv file to a dataframe
+    df = load_file_to_df(file_name, columns_IF)
 
-    #file.seek(0)
-    file = default_storage.open(file_name)
-    for chunk in pd.read_csv(file, chunksize=chunk_size, low_memory=False, usecols=columns_IF):
-        #chunk.columns = columns
-        #df = filter_columns(chunk, columns_IF)
-        df = chunk
-        clean_df(df)
-        df_p = pd.concat([df_p, df])
+    # Encodes labels
+    les = encode_labels(df)
 
-    les = encode_labels(df_p)
+    # Trains model and applies it to the dataframe
+    train_model(df, model)
+    apply_model(df, model)
 
-    train_model(df_p, model)
-    apply_model(df_p, model)
+    # Decodes labels
+    decode_labels(df, les)
 
-    decode_labels(df_p, les)
-
-    print(df_p)
-    df_p.to_csv(file_name.replace('.csv', '_processed.csv'), index=False)
-
-    file.close()
-    del file
-
-    default_storage.delete(file_name)
+    # Uploads results to database
+    upload_to_db(df, file_name.replace('.csv',''), base_file_name, date, internal_attributes, external_attributes)
+    
+    # Removes the file from the queue of files to process
     queue = np.array([])
-    if not exists('queue.npy'):
-        queue = np.delete(queue, 0)
-    else:
+    if exists('queue.npy'):
         queue = np.load('queue.npy')
         queue = np.delete(queue, 0)
     
+    # Saves the updated queue
     saved = False
     while (not saved):
         try:
@@ -165,8 +192,8 @@ def process_file(file_name, columns_IF):
             saved = True
         except:
             print('Error saving queue, trying again...')
+            
+    df.to_csv(file_name.replace('.csv','')+'_processed.csv', index=False)
     
+    # Sleeps for a while to avoid overloading the server
     time.sleep(2)
-    
-    #df_p.to_csv('test.csv')
-    #upload_to_db(df_p, db["File"])
