db54464fb47c08db93a528c98200d906940ae874
juangomez0120 <jmgomezortiz@gmail.com>
2022-05-22 18:00:53 -0500

    cambios front

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,38 +1,82 @@
 import React from 'react'
-import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, Area, AreaChart } from 'recharts'
 import { Doughnut } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const Graph = ({atributo1, chart, data}) => {
-    let graph
-    if (chart === 'Grafico de Barras'){
-        graph = 
-        <BarChart width={500} height={300} data = {data}>
-            <XAxis dataKey={atributo1} />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey='anomalias' fill='#ffa82f' />
-        </BarChart>
-    }
-    else if (chart === 'Grafico de Area') {
-        graph =
-        <AreaChart
-          width={500}
-          height={300}
-          data={data}
-        >
-          <XAxis dataKey={atributo1} />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="anomalias" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
-    }
-    else if (chart === 'Grafico de Burbuja'){
-        graph = <p>burbuja</p>
-    }
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['23', '543', '543', '234', '54', '234', '123'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 2, 3, 4, 5, 6, 7],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
+
+const Graph = () => {
+    // let graph
+    // if (chart === 'Grafico de Barras'){
+    //     graph = 
+    //     <BarChart width={500} height={300} data = {data}>
+    //         <XAxis dataKey={atributo1} />
+    //         <YAxis />
+    //         <Tooltip />
+    //         <Bar dataKey='anomalias' fill='#ffa82f' />
+    //     </BarChart>
+    // }
+    // else if (chart === 'Grafico de Area') {
+    //     graph =
+    //     <AreaChart
+    //       width={500}
+    //       height={300}
+    //       data={data}
+    //     >
+    //       <XAxis dataKey={atributo1} />
+    //       <YAxis />
+    //       <Tooltip />
+    //       <Area type="monotone" dataKey="anomalias" stroke="#8884d8" fill="#8884d8" />
+    //     </AreaChart>
+    // }
+    // else if (chart === 'Grafico de Burbuja'){
+    //     graph = <p>burbuja</p>
+    // }
 
   return (
-      <ResponsiveContainer width='100%' height='100%'>{graph}</ResponsiveContainer>
+    <Bar options={options} data={data} />
+      // <ResponsiveContainer width='100%' height='100%'>{graph}</ResponsiveContainer>
   )
 }
 
