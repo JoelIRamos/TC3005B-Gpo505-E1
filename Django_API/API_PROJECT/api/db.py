c93d77a9794012c6947b747fac6b8e39bbf04f69
Joel I. Ramos <joeliramosh@hotmail.com>
2022-05-23 18:08:37 -0500

    New DB

--- Django_API/API_PROJECT/api/db.py
+++ Django_API/API_PROJECT/api/db.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 # Conexión a la base de datos
 cluster = MongoClient("mongodb+srv://JoelIRamosH:JoelI@cluster0.htyzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = cluster["TernData"]
+db = cluster["TerniumData"]
